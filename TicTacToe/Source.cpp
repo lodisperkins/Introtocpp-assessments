5967ec1b779a0d363203536ebec50863b9f66a72
Lodis Perkins <s188043@students.aie.edu>
2018-09-06 14:20:13 -0500

    shit

--- TicTacToe/Source.cpp
+++ TicTacToe/Source.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 #include <string>
+int sumnumbas(int a, int b, int c)
+{
+	int result = 0;
+	result = a + b + c;
+	return result;
+}
 int main()
 {
 	char grid[3][3] = { {'a','a','a'}, {'a','a','a'}, {'a','a','a'} };
 	//these are the coordinates that the user will input to decide where to go
 	int x;
 	int y;
-	//this is what is used to determined who's turne it is.
+	//this is what is used to determined who's turn it is.
 	bool turnx;
 	bool turno;
 	//integers to iterate of array
 	int i;
 	int j;
 	int temp;
+	int turns = 0;
 	//this is the layout for the grid. the plan is to have the user change the output of grid in the cooridinate they want
 	do
 	{
@@ -30,7 +37,8 @@ int main()
 			std::cout << "x's turn\n";
 			for (turnx; turnx = true;)
 			{
-				std::cin >> x; std::cout << ", "; std::cin >> y;
+				std::cout << "Enter a coordinate ";
+				std::cin >> x >> y;
 				if (grid[x][y] == 'a')
 				{
 					grid[x][y] = 'x';
@@ -44,6 +52,52 @@ int main()
 					continue;
 				}
 			}
+		}
+		system("cls");
+		std::cout << "     0        1         2\n";
+		std::cout << "        " << grid[0][0] << "|       " << grid[0][1] << "|   " << grid[0][2] << std::endl;
+		std::cout << "0" << "   _____|________|_____\n";
+		std::cout << "        " << grid[1][0] << "|       " << grid[1][1] << "|   " << grid[1][2] << std::endl;
+		std::cout << "1" << "   _____|________|_____\n";
+		std::cout << "2" << "        |        |        \n";
+		std::cout << "        " << grid[2][0] << "|       " << grid[2][1] << "|   " << grid[2][2] << std::endl;
+		bool victoryMetX = false;
+		if (((grid[0][0] == 'x') && (grid[0][1] == 'x')) && (grid[0][2] == 'x'))
+		{
+			victoryMetX = true;
+		}
+		else if (((grid[1][0] == 'x') && (grid[1][1] == 'x')) && (grid[1][2] == 'x'))
+		{
+			victoryMetX = true;
+		}
+		else if (((grid[2][0] == 'x') && (grid[2][1] == 'x')) && (grid[2][2] == 'x'))
+		{
+			victoryMetX = true;
+		}
+		else if (((grid[0][0] == 'x') && (grid[1][0] == 'x')) && (grid[2][0] == 'x'))
+		{
+			victoryMetX = true;
+		}
+		else if (((grid[0][1] == 'x') && (grid[1][1] == 'x')) && (grid[2][1] == 'x'))
+		{
+			victoryMetX = true;
+		}
+		else if (((grid[0][2] == 'x') && (grid[0][2] == 'x')) && (grid[2][2] == 'x'))
+		{
+			victoryMetX = true;
+		}
+		else if (((grid[0][0] == 'x') && (grid[1][1] == 'x')) && (grid[2][2] == 'x'))
+		{
+			victoryMetX = true;
+		}
+		else if (((grid[2][0] == 'x') && (grid[1][1] == 'x')) && (grid[0][2] == 'x'))
+		{
+			victoryMetX = true;
+		}
+		if (victoryMetX == true)
+		{
+			std::cout << " x wins!" << std::endl;
+			break;
 		}
 		//o's turn. o is asked to input their desired cooridinates however if the cooridinates given is not equal to the place holder or 'a' the loop asks for a different input
 		if (turno = true)
@@ -65,28 +119,56 @@ int main()
 				}
 			}
 		}
-		for (int j = 0; j <= 3; j++)
-		{  
-			int i = 0;
-			for (int i = 0; i <= 3; i++)
-			{
-				if (grid[j][i] == 'x')
-				{
-					continue;
-				}
-				else if ((grid[j][i] =='x') && (i==3)) 
-				{
-					std::cout << "x wins\n";
-					break;
-				}
-				else
-				{
-					break;
-				}
-			}
+		system("cls");
+		
+		bool victoryMetO = false;
+		if (((grid[0][0] == 'o') && (grid[0][1] == 'o')) && (grid[0][2] == 'o'))
+		{
+			victoryMetO = true;
+		}
+		else if (((grid[1][0] == 'o') && (grid[1][1] == 'o')) && (grid[1][2] == 'o'))
+		{
+			victoryMetO = true;
+		}
+		else if (((grid[2][0] == 'o') && (grid[2][1] == 'o')) && (grid[2][2] == 'o'))
+		{
+			victoryMetO = true;
+		}
+		else if (((grid[0][0] == 'o') && (grid[1][0] == 'o')) && (grid[2][0] == 'o'))
+		{
+			victoryMetO = true;
+		}
+		else if (((grid[0][1] == 'o') && (grid[1][1] == 'o')) && (grid[2][1] == 'o'))
+		{
+			victoryMetO = true;
+		}
+		else if (((grid[0][2] == 'o') && (grid[0][2] == 'o')) && (grid[2][2] == 'o'))
+		{
+			victoryMetO = true;
+		}
+		else if (((grid[0][0] == 'o') && (grid[1][1] == 'o')) && (grid[2][2] == 'o'))
+		{
+			victoryMetO = true;
+		}
+		else if (((grid[2][0] == 'o') && (grid[1][1] == 'o')) && (grid[0][2] == 'o'))
+		{
+			victoryMetO = true;
+		}
+		else if (victoryMetO == true)
+		{
+			std::cout << " o wins!" << std::endl;
+			break;
+		}
+		else if (((victoryMetX == false) && (victoryMetO == false)) && (turns >= 8))
+		{
+			std::cout << "Ya both lost\n";
+			break;
+		}
+		else if (((victoryMetX == false) && (victoryMetO == false)) && (turns < 8))
+		{
+			turns++;			
 		}
 		
-		continue;
 	} while (true);
 	system("pause");
 }
